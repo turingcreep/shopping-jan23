6c1ed98a6164c4d15835b52d97cc7b8c22ff8f4a
Shankar Sengupta <shankar.sengupta.dev@gmail.com>
2016-01-28 13:03:10 -0800

    Adding sass framework for stylesheet compilation. Now we can go ahead with creating our kata components.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var	gulp = require('gulp'),
+	sass = require('gulp-sass'),
 	react = require('gulp-react');
 
-gulp.task('dev',function(){
+gulp.task('jsx:dev',function(){
 	return gulp.src('components/*.jsx')
 			.pipe(react())
 			.pipe(gulp.dest('public/javascripts'));
 });
+gulp.task('sass:dev',function(){
+	return gulp.src('components/*.scss')
+			.pipe(sass())
+			.pipe(gulp.dest('public/stylesheets'));
+});
+gulp.task('dev',['jsx:dev','sass:dev']);
