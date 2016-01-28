84124d56be18c10ecad5d53b0550fb03ecd7da1d
Shankar Sengupta <shankar.sengupta.dev@gmail.com>
2016-01-28 13:31:50 -0800

    Adding watches Concatinating all jsx to app.js Compiling and concatinating all sass to app.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 var	gulp = require('gulp'),
+	concat = require('gulp-concat'),
 	sass = require('gulp-sass'),
 	react = require('gulp-react');
 
 gulp.task('jsx:dev',function(){
-	return gulp.src('components/*.jsx')
+	return gulp.src('components/**/*.jsx')
 			.pipe(react())
+			.pipe(concat('app.js'))
 			.pipe(gulp.dest('public/javascripts'));
 });
 gulp.task('sass:dev',function(){
-	return gulp.src('components/*.scss')
+	return gulp.src('components/**/*.scss')
 			.pipe(sass())
+			.pipe(concat('app.css'))
 			.pipe(gulp.dest('public/stylesheets'));
 });
-gulp.task('dev',['jsx:dev','sass:dev']);
+gulp.task('dev',['jsx:dev','sass:dev'],function(){
+	gulp.watch('components/**/*.jsx',['jsx:dev']);
+	gulp.watch('components/**/*.scss',['sass:dev']);
+});
